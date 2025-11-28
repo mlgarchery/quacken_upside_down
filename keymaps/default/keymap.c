61b26eb7b24d16217631d1ee3de2548c035ad178
mlgarchery <martial.garchery@gmail.com>
2025-11-28 22:39:41 +0100

    cleaning

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #include QMK_KEYBOARD_H
 
-// In keymap.c:
 #define LAYOUT_MIRROR_RIGHT( \
     L00, L01, L02, L03, L04, L05,       R00, R01, R02, R03, R04, R05, \
     L10, L11, L12, L13, L14, L15,       R10, R11, R12, R13, R14, R15, \
